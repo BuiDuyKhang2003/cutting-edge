79268cd7eab4ca3167e91f19d66d6d8e6505b582
BuiDuyKhang2003 <huvo201223@gmail.com>
2024-11-09 15:47:04 +0700

    fic deploy

--- src/main/java/com/exe201/apicontroller/OrderController.java
+++ src/main/java/com/exe201/apicontroller/OrderController.java
@@ -2,7 +2,7 @@ package com.exe201.apicontroller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.springboot.app.type.CreatePaymentLinkRequestBody;
+//import com.springboot.app.type.CreatePaymentLinkRequestBody;
 import org.springframework.web.bind.annotation.*;
 import vn.payos.PayOS;
 import vn.payos.type.CheckoutResponseData;
@@ -23,41 +23,41 @@ public class OrderController {
         this.payOS = payOS;
     }
 
-    @PostMapping(path = "/create")
-    public ObjectNode createPaymentLink(@RequestBody CreatePaymentLinkRequestBody RequestBody) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        ObjectNode response = objectMapper.createObjectNode();
-        try {
-            final String productName = RequestBody.getProductName();
-            final String description = RequestBody.getDescription();
-            final String returnUrl = RequestBody.getReturnUrl();
-            final String cancelUrl = RequestBody.getCancelUrl();
-            final int price = RequestBody.getPrice();
-            // Gen order code
-            String currentTimeString = String.valueOf(String.valueOf(new Date().getTime()));
-            long orderCode = Long.parseLong(currentTimeString.substring(currentTimeString.length() - 6));
-
-            ItemData item = ItemData.builder().name(productName).price(price).quantity(1).build();
-
-            PaymentData paymentData = PaymentData.builder().orderCode(orderCode).description(description).amount(price)
-                    .item(item).returnUrl(returnUrl).cancelUrl(cancelUrl).build();
-
-            CheckoutResponseData data = payOS.createPaymentLink(paymentData);
-
-            response.put("error", 0);
-            response.put("message", "success");
-            response.set("data", objectMapper.valueToTree(data));
-            return response;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            response.put("error", -1);
-            response.put("message", "fail");
-            response.set("data", null);
-            return response;
-
-        }
-    }
+//    @PostMapping(path = "/create")
+//    public ObjectNode createPaymentLink(@RequestBody CreatePaymentLinkRequestBody RequestBody) {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        ObjectNode response = objectMapper.createObjectNode();
+//        try {
+//            final String productName = RequestBody.getProductName();
+//            final String description = RequestBody.getDescription();
+//            final String returnUrl = RequestBody.getReturnUrl();
+//            final String cancelUrl = RequestBody.getCancelUrl();
+//            final int price = RequestBody.getPrice();
+//            // Gen order code
+//            String currentTimeString = String.valueOf(String.valueOf(new Date().getTime()));
+//            long orderCode = Long.parseLong(currentTimeString.substring(currentTimeString.length() - 6));
+//
+//            ItemData item = ItemData.builder().name(productName).price(price).quantity(1).build();
+//
+//            PaymentData paymentData = PaymentData.builder().orderCode(orderCode).description(description).amount(price)
+//                    .item(item).returnUrl(returnUrl).cancelUrl(cancelUrl).build();
+//
+//            CheckoutResponseData data = payOS.createPaymentLink(paymentData);
+//
+//            response.put("error", 0);
+//            response.put("message", "success");
+//            response.set("data", objectMapper.valueToTree(data));
+//            return response;
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            response.put("error", -1);
+//            response.put("message", "fail");
+//            response.set("data", null);
+//            return response;
+//
+//        }
+//    }
 
     @GetMapping(path = "/{orderId}")
     public ObjectNode getOrderById(@PathVariable("orderId") long orderId) {
